798102e23e7ea966c31a12c8b55613a31ead933b
Izhan-Imran <izhanimran14@gmail.com>
2024-04-09 14:21:39 +0500

    second commit

--- git-learning.js
+++ git-learning.js
@@ -1,1 +1,6 @@
 console.log("Hello World!");
+var a = 5;
+var b = 7;
+var sum = a + b;
+var message = "The sum of a + b is " + sum;
+console.log(message);
--- git-learning.ts
+++ git-learning.ts
@@ -1,1 +1,8 @@
 console.log("Hello World!");
+
+let a = 5;
+let b = 7;
+let sum = a + b;
+let message = "The sum of a + b is " + sum;
+
+console.log(message);
